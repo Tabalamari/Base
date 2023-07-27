c8893fcc3375576f10502637b95e72057b63686e
Tabalamari <tabalamari@gmail.com>
2023-07-27 12:16:16 +0100

    game v1 is done

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const location1 = 2;
 const location2 = 3;
 const location3 = 4;
 let guess; //номер поточної спроби
-const hits = 0; //кількість влучань
-const guesses = 0; //кількість спроб
-const isSunk = false; //потоплений корабель чи ні
+let hits = 0; //кількість влучань
+let guesses = 0; //кількість спроб
+let isSunk = false; //потоплений корабель чи ні
 
 while (isSunk == false) {
     guess = prompt("Введіть координати (enter a number 0-6):")
@@ -12,14 +12,20 @@ while (isSunk == false) {
         alert("Please enter a valid cell number!");
     } else {
         guesses = guesses + 1;
+        console.log(guesses)
     }
     if (guess == location1 || guess == location2 || guess == location3) {
         hits = hits + 1;
+        alert("HIT!")
     }
+    else {
+        alert("MISS")
+    }
+
     if (hits == 3) {
         isSunk = true;
         alert("You sank my battleship!")
     }
 }
-var stats = "You took " + guesses + " guesses to sink the battleship, " + alert(stats); "which means your shooting accuracy was " + (3 / guesses);
-
+var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accuracy was " + (3 / guesses);
+alert(stats);
