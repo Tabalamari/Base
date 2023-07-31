838994a081b6a7640b923972b976c09b3e9f5c5b
Tabalamari <tabalamari@gmail.com>
2023-07-31 20:17:10 +0100

    easy vers of game done

--- script.js
+++ script.js
@@ -2,22 +2,32 @@ let randomLock = Math.floor(Math.random() * 5);
 let location1 = randomLock;
 let location2 = randomLock + 1;
 let location3 = randomLock + 2;
-let guess; //номер поточної спроби
+let guess; //номер клітинки
 let hits = 0; //кількість влучань
 let guesses = 0; //кількість спроб
 let isSunk = false; //потоплений корабель чи ні
+let hitsArray = [];
 
 while (isSunk == false) {
     guess = prompt("Введіть координати (enter a number 0-6):")
     if (guess < 0 || guess > 6) {
         alert("Please enter a valid cell number!");
-    } else {
+    }
+    else {
         guesses = guesses + 1;
         console.log(guesses)
     }
     if (guess == location1 || guess == location2 || guess == location3) {
-        hits = hits + 1;
-        alert("HIT!")
+        let found = hitsArray.find(element => element === guess);
+        if (found) {
+            alert("In this cell was hit before")
+        }
+        else {
+            hits = hits + 1;
+            hitsArray.push(guess)
+            alert("HIT!")
+        }
+
     }
     else {
         alert("MISS")
