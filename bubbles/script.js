09f204f7efa7bab6eb4b631b334da5fa046b5cce
Tabalamari <tabalamari@gmail.com>
2023-08-12 20:34:57 +0100

    refactored part2

--- bubbles/script.js
+++ bubbles/script.js
@@ -1,54 +1,65 @@
 const scores1 = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18,
     41, 53, 55, 61, 51, 44];
-
+const costs = [.25, .27, .25, .25, .25, .25, .33, .31, .25, .29, .27, .22, .31, .25, .25, .33, .21, .25, .25, .25, .28, .25, .24, .22, .20, .25, .30, .25, .24, .25,
+    .25, .25, .27, .25, .26, .29];
 printReport(scores1)
 
 function printReport(scores) {
-    let bubb = document.getElementById("bubb");
-    bubb.innerHTML = bubbles(scores);
+    const bubbleSolutions = document.getElementById("bubbleSolutions");
+    bubbleSolutions.innerHTML = getRangeOfElements(scores, "Bubble Solution ").join("\n");
 
-    let tests = document.getElementById("tests");
+    const tests = document.getElementById("tests");
     tests.innerHTML = "<p style=display: flex>Bubbles tests: " + scores.length + "</p>";
 
-    let highestBubbleScore = document.getElementById("highestBubbleScore");
-    let highestScore = gethighestBubbleScore(scores);
+    const highestBubbleScore = document.getElementById("highestBubbleScore");
+    const highestScore = gethighestBubbleScore(scores);
     highestBubbleScore.innerHTML = "Highest bubble score: " + highestScore;
-    
-    let indexOfBiggestValue = document.getElementById("indexOfBiggestValue")
-    indexOfBiggestValue.innerHTML = getIndexOfBiggestValue(scores, highestScore);
+
+    const indexesOfHighestScore = document.getElementById("indexesOfHighestScore")
+    indexesOfHighestScore.innerHTML = "Solutions with highest score: " + getIndexOfValue(scores, highestScore);
+
+    const mostEffectiveIndex = document.getElementById("mostEffectiveIndex");
+    mostEffectiveIndex.innerHTML = "The most cost effective index of solution: " + "# " + getMostCostEffectiveSolution(scores, costs, highestScore)
 }
 
-function bubbles(scores) {
-    let result = [];
+function getRangeOfElements(scores, label) {
+    const result = [];
     for (let i = 0; i < scores.length; i++) {
         const element = scores[i];
-        result.push("<p>Bubble solution #" + i + " score:" + element + "</p>");
+        result.push("<p>" + label + "#" + i + " score:" + element + "</p>");
     }
-    return result.join("\n");
+    return result;
 }
-//var output;
-// var i = 0;
-// while (i < scores.length) {
-//     output = "Bubble solution #" + i + " score: " + scores[i]; console.log(output);
-//     i = i + 1;
 
 function gethighestBubbleScore(scores) {
-    function compareNumbers(a, b) {
-        return a - b;
-    }
-    let copyScores = scores.slice();
-    let sortScores = copyScores.sort(compareNumbers)
-    let highScores = sortScores[sortScores.length - 1];
+
+    const copyScores = scores.slice();
+    const sortScores = copyScores.sort((a, b) => a - b);
+    const highScores = sortScores[sortScores.length - 1];
 
     return highScores;
 }
 
-function getIndexOfBiggestValue(arr, val) {
+function getIndexOfValue(arr, val) {
     const indexes = [];
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] === val) {
             indexes.push(" #" + i);
         }
     }
-    return "Solutions with highest score: " + indexes;
+    return indexes;
+}
+
+function getMostCostEffectiveSolution(scores, costs, highscore) {
+    let cost = 100;
+    let index;
+    for (let i = 0; i < scores.length; i++) {
+        if (scores[i] === highscore) {
+            if (cost > costs[i]) {
+                index = i;
+                cost = costs[i];
+            }
+        }
+    }
+    return index;
 }
